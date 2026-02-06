36068f784a1610e4fad1191ef0bd435474c35bb3
koichi <111332153+koichi2426@users.noreply.github.com>
2026-02-06 23:32:52 +0900

    Initial Commit

---
+++ Iris Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iris Watch App
+//
+//  Created by 佐藤幸一 on 2026/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by 佐藤幸一 on 2026/02/06.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
